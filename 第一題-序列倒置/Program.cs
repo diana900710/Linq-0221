2a69b1ad4cd0b5db78cb9136ab86346824cd618a
樂門樂曼 <diana900710@gmail.com>
2023-02-21 11:34:49 +0800

    加入專案檔案。

---
+++ 第一題-序列倒置/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第一題_序列倒置
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一組數字(用 , 分隔) : ");
+            string result = Console.ReadLine();
+            var nums = result.Split(',');
+            var swap = nums.OrderByDescending((x) => x); //排序(遞減)
+            foreach ( var i in swap )
+            {
+                Console.Write("{0},", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 第二題-奇偶判斷/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第二題_奇偶判斷
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一串正整數(用 , 分隔) : ");
+            var result = Console.ReadLine().Split(',');
+            var nums = Array.ConvertAll(result, (x) => int.Parse(x)); //轉成數字陣列
+            var cardinality = nums.Where((x) => x % 2 == 1).OrderBy((x) => x);
+            var even = nums.Except(cardinality).OrderBy((x) => x);
+            Console.Write($"奇數 : ");
+            foreach(var item in cardinality) { Console.Write("{0},", item);}
+            Console.WriteLine();
+            Console.Write($"偶數 : ");
+            foreach (var item in even) { Console.Write("{0},", item); }
+
+            Console.ReadKey();
+        }
+    }
+}
